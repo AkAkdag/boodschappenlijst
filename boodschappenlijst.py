9a90211ab6eeb6b81ba5ba4c71dffca02390b97b
AkAkdag <125441510+AkAkdag@users.noreply.github.com>
2023-12-14 09:45:06 +0100

    Update boodschappenlijst.py

--- boodschappenlijst.py
+++ boodschappenlijst.py
@@ -4,28 +4,32 @@
 # Het gebruik van OOP draagt bij aan een efficiënte ontwikkeling en onderhoud van de software, terwijl het ook de leesbaarheid en uitbreidbaarheid bevordert.
 
 
-
 import tkinter as tk
 from tkinter import messagebox
 
 class BoodschappenLijst:
     def __init__(self):
-        self.artikellen = []
+        self.artikelen = {}
 
-    def voeg_artikel_toe(self, artikel):
-        self.artikellen.append(artikel)
+    def voeg_artikel_toe(self, artikel, hoeveelheid=1):
+        if artikel in self.artikelen:
+            self.artikelen[artikel] += hoeveelheid
+        else:
+            self.artikelen[artikel] = hoeveelheid
 
-    def verwijder_artikel(self, artikel):
-        if artikel in self.artikellen:
-            self.artikellen.remove(artikel)
+    def verwijder_artikel(self, artikel, hoeveelheid=1):
+        if artikel in self.artikelen:
+            self.artikelen[artikel] -= hoeveelheid
+            if self.artikelen[artikel] <= 0:
+                del self.artikelen[artikel]
             return True
         return False
 
     def leegmaken_lijst(self):
-        self.artikellen.clear()
+        self.artikelen.clear()
 
     def toon_lijst(self):
-        return self.artikellen
+        return self.artikelen
 
 class BoodschappenLijstAppTkinter:
     def __init__(self, master):
@@ -36,21 +40,21 @@ class BoodschappenLijstAppTkinter:
 
         self.create_widgets()
 
-
-
     def create_widgets(self):
-
-        
-        self.label = tk.Label(self.master, text="Boodschappenlijst", fg="blue", bg="lightblue", font= "5" )
+        self.label = tk.Label(self.master, text="Boodschappenlijst", fg="blue", bg="lightblue", font="5")
         self.label.pack()
 
-
-
         # ronde buttons
-        ronde_button_style = {"borderwidth": 2, "relief": tk.GROOVE, "width": 12, "height": 2, "bg": "blue", "fg": "white", }
-        
+        ronde_button_style = {"borderwidth": 2, "relief": tk.GROOVE, "width": 12, "height": 2, "bg": "blue", "fg": "white"}
+
         self.entry = tk.Entry(self.master)
         self.entry.pack()
+
+        self.quantity_label = tk.Label(self.master, text="Hoeveelheid:")
+        self.quantity_label.pack()
+
+        self.quantity_entry = tk.Entry(self.master)
+        self.quantity_entry.pack()
 
         self.add_button = tk.Button(self.master, text="Voeg toe", command=self.voeg_artikel_toe)
         self.add_button.config(**ronde_button_style)
@@ -78,24 +82,28 @@ class BoodschappenLijstAppTkinter:
 
     def voeg_artikel_toe(self):
         artikel = self.entry.get().upper()
-        self.boodschappen_lijst.voeg_artikel_toe(artikel)
-        messagebox.showinfo("Succes", f"{artikel} is aan de lijst toegevoegd")
+        hoeveelheid = int(self.quantity_entry.get()) if self.quantity_entry.get() else 1
+        self.boodschappen_lijst.voeg_artikel_toe(artikel, hoeveelheid)
+        messagebox.showinfo("Succes", f"{hoeveelheid} x {artikel} is aan de lijst toegevoegd")
         self.entry.delete(0, tk.END)
+        self.quantity_entry.delete(0, tk.END)
 
     def toon_lijst(self):
         lijst = self.boodschappen_lijst.toon_lijst()
         if not lijst:
             self.list_label.config(text="U heeft nul artikelen in uw lijst")
         else:
-            self.list_label.config(text="Artikelen in de lijst:\n" + "\n".join(lijst))
+            self.list_label.config(text="Artikelen in de lijst:\n" + "\n".join([f"{artikel}: {hoeveelheid}" for artikel, hoeveelheid in lijst.items()]))
 
     def verwijder_artikel(self):
         artikel = self.entry.get().upper()
-        if self.boodschappen_lijst.verwijder_artikel(artikel):
-            messagebox.showinfo("Succes", f"{artikel} is van de lijst verwijderd")
+        hoeveelheid = int(self.quantity_entry.get()) if self.quantity_entry.get() else 1
+        if self.boodschappen_lijst.verwijder_artikel(artikel, hoeveelheid):
+            messagebox.showinfo("Succes", f"{hoeveelheid} x {artikel} is van de lijst verwijderd")
         else:
             messagebox.showwarning("Waarschuwing", "Dat artikel staat niet in de lijst")
         self.entry.delete(0, tk.END)
+        self.quantity_entry.delete(0, tk.END)
 
     def wis_lijst(self):
         self.boodschappen_lijst.leegmaken_lijst()
